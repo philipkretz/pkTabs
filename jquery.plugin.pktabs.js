93854d83849b00c40aad123fd461c0227c5b5ce4
philipkretz <ich@philipkretz.de>
2014-05-29 01:10:03 +0200

    Update jquery.plugin.pktabs.js

--- jquery.plugin.pktabs.js
+++ jquery.plugin.pktabs.js
@@ -11,57 +11,52 @@
  */
  
  (function($) {
-	$.fn.extend({
-		pkTabs:function(params) {
-			
-			// Setting and extending initial values...
-			var conf = {
-					tabClass: "tab",
-					navigatorClass: "tab_links",
-					showIndex: 0
-				};
-			$.extend(conf, params);
-			
-			var elem = $(this);
-			
-			function init() {
-				return $(this).each(function(){
-					var elCounter = 0;
-					elem.find("."+conf.tabClass).each(function() {
-						if (elCounter == conf.showIndex) $(this).show();
-						else $(this).hide();
-						elCounter++;
-					});
-					elCounter=0;
-					elem.find("."+conf.navigatorClass+">a").each(function() {
-						if (elCounter == conf.showIndex) $(this).css("text-decoration", "none");
-						else $(this).css("text-decoration", "underline");
-						elCounter++;
-						$(this).click(function() {
-							selectTab($(this).attr("href"));
-						});
-					});
+	$.fn.pkTabs = function(params) {
+		
+		// Setting and extending initial values...
+		var conf = {
+				tabClass: "tab",
+				navigatorClass: "tab_links",
+				showIndex: 0
+			};
+		$.extend(conf, params);
+		
+		var elem = $(this);
+		return $(this).each(function(){
+		function init() {
+			var elCounter = 0;
+			elem.find("."+conf.tabClass).each(function() {
+				if (elCounter == conf.showIndex) $(this).show();
+				else $(this).hide();
+				elCounter++;
+			});
+			elCounter=0;
+			elem.find("."+conf.navigatorClass+">a").each(function() {
+				if (elCounter == conf.showIndex) $(this).css("text-decoration", "none");
+				else $(this).css("text-decoration", "underline");
+				elCounter++;
+				$(this).click(function() {
+					selectTab($(this).attr("href"));
 				});
-			}
-			
-			function selectTab(tabId) {
-				return $(this).each(function(){
-					elem.find("."+conf.tabClass).each(function() {
-						if (tabId == "#"+$(this).attr("id")) $(this).show();
-						else $(this).hide();
-					});
-					elem.find("."+conf.navigatorClass+">a").each(function() {
-						if (tabId == $(this).attr("href")) {
-							$(this).css("text-decoration", "none");
-						}
-						else {
-							$(this).css("text-decoration", "underline");
-						}
-					});
-				});
-			}
-			
-			init();
+			});
 		}
-	});
+		
+		function selectTab(tabId) {
+			elem.find("."+conf.tabClass).each(function() {
+				if (tabId == "#"+$(this).attr("id")) $(this).show();
+				else $(this).hide();
+			});
+			elem.find("."+conf.navigatorClass+">a").each(function() {
+				if (tabId == $(this).attr("href")) {
+					$(this).css("text-decoration", "none");
+				}
+				else {
+					$(this).css("text-decoration", "underline");
+				}
+			});
+		}
+		
+		init();
+		});
+	}
  })(jQuery);
