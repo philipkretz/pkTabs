3443053473216072902d2d3e34ddedb7b1be9df8
philipkretz <ich@philipkretz.de>
2014-05-29 13:10:13 +0200

    new files

--- jquery.plugin.pktabs.js
+++ jquery.plugin.pktabs.js
@@ -1,7 +1,7 @@
 /*
  * 
  * pkTabs - A simple tab group for most use cases.
- * Version 0.1
+ * Version 0.1.0
  * @requires jQuery v1.5
  * 
  * Copyright (c) 2014 Philip Kretz
@@ -31,9 +31,16 @@
 				elCounter++;
 			});
 			elCounter=0;
+			elem.find("."+conf.navigatorClass+">a").addClass("tabLink");
 			elem.find("."+conf.navigatorClass+">a").each(function() {
-				if (elCounter == conf.showIndex) $(this).css("text-decoration", "none");
-				else $(this).css("text-decoration", "underline");
+				if (elCounter == conf.showIndex) {
+					$(this).removeClass("tabLinkHide");
+					$(this).addClass("tabLinkShow");
+				}
+				else {
+					$(this).removeClass("tabLinkShow");
+					$(this).addClass("tabLinkHide");
+				}
 				elCounter++;
 				$(this).click(function() {
 					selectTab($(this).attr("href"));
@@ -48,10 +55,12 @@
 			});
 			elem.find("."+conf.navigatorClass+">a").each(function() {
 				if (tabId == $(this).attr("href")) {
-					$(this).css("text-decoration", "none");
+					$(this).removeClass("tabLinkHide");
+					$(this).addClass("tabLinkShow");
 				}
 				else {
-					$(this).css("text-decoration", "underline");
+					$(this).removeClass("tabLinkShow");
+					$(this).addClass("tabLinkHide");
 				}
 			});
 		}
